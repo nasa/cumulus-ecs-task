391a35aa6c653031ca94719485edeab2d305b584
Mark Boyd <markdavidboyd@gmail.com>
2020-07-10 12:47:40 -0400

    add script and CI step to do project releases

--- bin/release.sh
+++ bin/release.sh
@@ -1,19 +1,31 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-IMAGE=cumuluss/cumulus-ecs-task
-VERSION=$(cat package.json | node_modules/.bin/jsonfilter version | sed -e 's/^"//' -e 's/"$//')
-PREVIOUS_VERSION=$(npm view @cumulus/cumulus-ecs-task version)
+set -ex
+export VERSION_NUMBER=$(jq --raw-output .version package.json)
+export VERSION_TAG="v$VERSION_NUMBER"
+export LATEST_TAG=$(curl -H \
+  "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/nasa/cumulus-ecs-task/tags | jq --raw-output '.[0].name')
 
-set -o pipefail
+if [ "$VERSION_TAG" != "$LATEST_TAG" ]; then
+  echo "tag does not exist for version $VERSION_TAG, creating tag"
 
-if [ "$VERSION" = "$PREVIOUS_VERSION" ]; then
-  echo "$VERSION already released"
-  exit 0
+  # create git tag
+  git tag -a "$VERSION_TAG" -m "$VERSION_TAG" || echo "$VERSION_TAG already exists"
+  git push origin "$VERSION_TAG"
 fi
 
-if [ "$VERSION" != "$PREVIOUS_VERSION" ]; then
-  npm publish --access public
-  docker login -u cumulususer -p $DOCKER_PASSWORD
-  docker tag $IMAGE:latest $IMAGE:$VERSION
-  docker push $IMAGE:$VERSION
+export RELEASE_URL=$(curl -H \
+  "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/nasa/cumulus-ecs-task/releases/tags/$VERSION_TAG | jq --raw-output '.url // ""')
+
+if [ -z "$RELEASE_URL" ]; then
+  echo "release does not exist"
+
+  curl -H \
+    "Authorization: token $GITHUB_TOKEN" \
+    -d "{\"tag_name\": \"$VERSION_TAG\", \"name\": \"$VERSION_TAG\", \"body\": \"Release $VERSION_TAG\" }"\
+    -H "Content-Type: application/json"\
+    -X POST \
+    https://api.github.com/repos/nasa/cumulus-ecs-task/releases
 fi
